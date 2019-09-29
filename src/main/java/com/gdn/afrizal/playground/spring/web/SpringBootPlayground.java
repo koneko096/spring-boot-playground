ae823a6b295d4e6c22ce403632cc295297af9f16
Afrizal Fikri <laser.survivor@gmail.com>
2019-09-30 00:07:46 +0700

    Update config

--- src/main/java/com/gdn/afrizal/playground/spring/web/SpringBootPlayground.java
+++ src/main/java/com/gdn/afrizal/playground/spring/web/SpringBootPlayground.java
@@ -1,21 +1,18 @@
 package com.gdn.afrizal.playground.spring.web;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.gdn.afrizal.playground.spring.client")
 @EnableResourceServer
+@Configuration
 public class SpringBootPlayground extends ResourceServerConfigurerAdapter {
-
-    @Value("${security.oauth2.resource.id}")
-    private String resourceId;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPlayground.class, args);
@@ -25,10 +22,5 @@ public class SpringBootPlayground extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated()
             .anyRequest().permitAll();
-    }
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer oauthServer) {
-        oauthServer.resourceId(resourceId);
     }
 }
